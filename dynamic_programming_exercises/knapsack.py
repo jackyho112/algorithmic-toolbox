aca0bd3aa4ac5c8b7e788df2431fd7890551eb0b
jackyho112 <jackyho112dev@gmail.com>
2017-08-17 14:13:05 -0700

    Finish another dynamic programming examples

--- dynamic_programming_exercises/knapsack.py
+++ dynamic_programming_exercises/knapsack.py
@@ -2,12 +2,34 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    results = [
+        [0] * W
+    ]
+    w = sorted(w)
+
+    for index in range(len(w)):
+        row = []
+        current_weight = w[index]
+        for weight in range(1, W + 1):
+            if weight >= current_weight:
+                remaining_weight = weight - current_weight
+
+                if remaining_weight > 0:
+                    row.append(
+                        max(
+                            current_weight + results[index][remaining_weight - 1],
+                            results[index][weight - 1]
+                        )
+                    )
+                else:
+                    row.append(current_weight)
+            else:
+                row.append(results[index][weight - 1])
+
+        results.append(row)
+
+    return results[-1][-1]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
